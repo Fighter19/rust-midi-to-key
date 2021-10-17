be38715af67dfc4155ed3ac7445525128ad29d93
Fighter19 <1475802+Fighter19@users.noreply.github.com>
2021-10-17 02:53:38 +0200

    Starting to unify the main function

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::thread::sleep;
 use std::time::Duration;
 use std::io::{stdin, stdout, Write};
 
-use midir::{MidiInput, Ignore};
+use midir::{ConnectError, Ignore, MidiInput, MidiInputConnection};
 
 struct KeyboardMsg {
     is_press: bool,
@@ -15,7 +15,7 @@ struct KeyboardMsg {
     press_shift: bool
 }
 
-fn receive_midi_msg_for_device(device: evdev::uinput::VirtualDevice, stamp: u64, message: &[u8]) {
+fn receive_midi_msg_for_device(device: evdev::uinput::VirtualDevice, _stamp: u64, message: &[u8]) {
     if(message[0] == 144) {
         // 75 = D#5
         // 72 => C5
@@ -32,7 +32,7 @@ fn receive_midi_msg_for_device(device: evdev::uinput::VirtualDevice, stamp: u64,
     }
 }
 
-const button_lut: [evdev::Key; 12] = [
+const BUTTON_LUT: [evdev::Key; 12] = [
     Key::KEY_Q,
     Key::KEY_2,
     Key::KEY_W,
@@ -51,53 +51,24 @@ fn generate_button_press(mut device: evdev::uinput::VirtualDevice, keyboard_msg:
     //println!("Pressed button: {}", keyboard_msg.button_to_press);
     //println!("Would press button: {}", button_lut[keyboard_msg.button_to_press as usize]);
     let type_ = EventType::KEY;
-    let press_event = InputEvent::new(type_, button_lut[keyboard_msg.button_to_press as usize].code(), keyboard_msg.is_press as i32);
+    let press_event = InputEvent::new(type_, BUTTON_LUT[keyboard_msg.button_to_press as usize].code(), keyboard_msg.is_press as i32);
     device.emit(&[press_event]).unwrap();
 }
 
-fn main() -> Result<()> {
-    let device = Device::open("/dev/input/event9")?;
-    // This is example from the documentation of the evdev crate, checks if device has an enter key
-    if device.supported_keys().map_or(false, |keys| keys.contains(Key::KEY_ENTER)) {
-        println!("are you prepared to ENTER the world of evdev?");
-    } else {
-        println!(":(");
-    }
-    Ok(())
-}
-
-#[test]
-fn test_create_device() -> Result<()> {
+fn initialize_kbd_device() -> Result<evdev::uinput::VirtualDevice, std::io::Error> {
     let mut keys = AttributeSet::<Key>::new();
-    keys.insert(Key::KEY_Z);
+    for key in BUTTON_LUT.iter() {
+        keys.insert(*key);
+    }
 
     let mut device = VirtualDeviceBuilder::new()?
         .name("Fake Keyboard")
         .with_keys(&keys)?
-        .build()
-        .unwrap();
-
-    let type_ = EventType::KEY;
-    // Note this will ACTUALLY PRESS the button on your computer.
-    // Hopefully you don't have BTN_DPAD_UP bound to anything important.
-    //let code = Key::BTN_DPAD_UP.code();
-    let code = Key::KEY_Z.code();
-
-    sleep(Duration::from_secs(2));
-    let down_event = InputEvent::new(type_, code, 1);
-    device.emit(&[down_event]).unwrap();
-    println!("Pressed.");
-    sleep(Duration::from_secs(2));
-
-    let up_event = InputEvent::new(type_, code, 0);
-    device.emit(&[up_event]).unwrap();
-    println!("Released.");
-    sleep(Duration::from_secs(2));
-    Ok(())
+        .build();
+    return device;
 }
 
-#[test]
-fn test_midir() -> Result<()> {
+fn initialize_midi_device() -> Result<MidiInputConnection<()>> {
     let mut input = String::new();
 
     let mut midi_in = MidiInput::new("midir reading input")?;
@@ -137,12 +108,67 @@ fn test_midir() -> Result<()> {
     if _conn_in.is_err() {
         println!("Failed to open MIDI Input connection");
     }
-    
-    println!("Connection open, reading input from '{}' (press enter to exit) ...", in_port_name);
 
+    println!("Connection open, reading input from '{}' (press enter to exit) ...", in_port_name);
     input.clear();
+    match _conn_in {
+        Ok(con) => Ok(con),
+        Err(why) => Err(anyhow::anyhow!("Unable to open Midi Input Connection"))
+    }
+
+}
+
+// impl From<ConnectError<MidiInput>> for anyhow::Error {
+//     fn from(error: ConnectError<MidiInput>) {
+//         anyhow::anyhow!("Failed to connect Midi Input")
+//     } 
+// }
+
+fn main() -> Result<()> {
+    let mut device = initialize_kbd_device()?;
+    let midi_device = initialize_midi_device()?;
+    let mut input = String::new();
     stdin().read_line(&mut input)?; // wait for next enter key press
 
+    Ok(())
+}
+
+#[test]
+fn test_create_device() -> Result<()> {
+    let mut keys = AttributeSet::<Key>::new();
+    keys.insert(Key::KEY_Z);
+
+    let mut device = VirtualDeviceBuilder::new()?
+        .name("Fake Keyboard")
+        .with_keys(&keys)?
+        .build()
+        .unwrap();
+
+    let type_ = EventType::KEY;
+    // Note this will ACTUALLY PRESS the button on your computer.
+    // Hopefully you don't have BTN_DPAD_UP bound to anything important.
+    //let code = Key::BTN_DPAD_UP.code();
+    let code = Key::KEY_Z.code();
+
+    sleep(Duration::from_secs(2));
+    let down_event = InputEvent::new(type_, code, 1);
+    device.emit(&[down_event]).unwrap();
+    println!("Pressed.");
+    sleep(Duration::from_secs(2));
+
+    let up_event = InputEvent::new(type_, code, 0);
+    device.emit(&[up_event]).unwrap();
+    println!("Released.");
+    sleep(Duration::from_secs(2));
+    Ok(())
+}
+
+#[test]
+fn test_midir() -> Result<()> {
+    let midi_device = initialize_midi_device()?;
+
+    let mut input = String::new();
+    stdin().read_line(&mut input)?; // wait for next enter key press
     println!("Closing connection");
     Ok(())
 }
